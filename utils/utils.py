3a30f0dbb9b382ac4bd5f5b18119f8d779ff7af2
Mihail <vislapyz@mail.ru>
2024-01-23 22:59:54 +0300

    the first function download_operation

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,12 @@
 import json
-import datetime
+
 
 def download_operation(filename):
     """загружает файл из operations.json
+       дата из файла
     """
     with open(filename, 'r', encoding='utf-8') as file:
         operations = json.load(file)
+
+    operation_data = sorted(operations, key=lambda x: x.get('date', ""), reverse=True)
+    return operation_data
